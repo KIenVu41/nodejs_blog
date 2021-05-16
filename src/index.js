9afce8e7371a95067e000cbb6bd3f9edff011f72
Vu Trung Kien <kienvutrung20@gmail.com>
2021-05-16 15:38:43 +0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ const exphbs  = require('express-handlebars');
 const path = require('path');
 const app = express();
 const port = 3000;
-
+const route = require('./routes')
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
 
 // HTTP logger
 app.use(morgan('combined'));
@@ -15,16 +20,12 @@ app.use(morgan('combined'));
 app.engine('hbs', exphbs({
   extname:".hbs"
 }));
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/trang-chu', (req, res) => {
-  res.render('home');
-});
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+// Route init
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
